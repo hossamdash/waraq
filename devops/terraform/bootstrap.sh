691bf59d27e1cdd1adc7d17db843a743c87c22b9
Hossam Mostafa <hossamdash2@gmail.com>
2026-01-12 22:57:06 +0200

    feat(terraform): consolidate bootstrap process by merging scripts and removing redundant files

--- devops/terraform/bootstrap.sh
+++ devops/terraform/bootstrap.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
-./start_localstack.sh
-./create_state_bucket.sh
+
+# Start k3s service
+systemctl start k3s.service
+
+# Start LocalStack
+localstack start --detached
+
+# Create S3 state bucket
+PROFILE="localstack"
+REGION="eu-west-1"
+BUCKET="waraq-terraform-state-$REGION"
+
+aws --profile "$PROFILE" s3api create-bucket \
+    --bucket "$BUCKET" \
+    --region "$REGION" \
+    --create-bucket-configuration LocationConstraint="$REGION"
+
+aws --profile "$PROFILE" s3api put-bucket-versioning \
+    --bucket "$BUCKET" \
+    --versioning-configuration Status=Enabled
--- devops/terraform/start_localstack.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-localstack start --detached
