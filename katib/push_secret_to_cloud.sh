d51bfabd47f1d6422e8964ed0c1966243f3a4b73
Hossam Mostafa <hossamdash2@gmail.com>
2026-01-15 00:59:04 +0200

    chore: enable push secret to update existing secret

--- katib/push_secret_to_cloud.sh
+++ katib/push_secret_to_cloud.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # in actual production this would upload to AWS secret manager. here the vault namespace is a simulation of that.
 # you must have the local .env file ready before running this. it's not tracked in git for security reasons.
-kubectl --namespace vault create secret generic katib --from-file=.env=.env
+kubectl --namespace vault create secret generic katib --from-file=.env=.env --dry-run=client -o yaml | kubectl apply -f -
